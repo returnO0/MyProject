9b2f8853a4101e6e28a7bbf833129a5122395303
zhiqiang.hu <zhiqiang.hu01@hand-china.com>
2019-12-16 20:25:45 +0800

    基础配置

--- src/test/java/com/hzq/crud/CrudApplicationTests.java
+++ src/test/java/com/hzq/crud/CrudApplicationTests.java
@@ -1,13 +1,25 @@
 package com.hzq.crud;
 
+import com.hzq.springbootweb.CrudApplication;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@SpringBootTest(classes = CrudApplication.class)
 class CrudApplicationTests {
 
+    @Autowired
+    DataSource dataSource;
     @Test
-    void contextLoads() {
+    void contextLoads() throws SQLException {
+        System.out.println("1111111111111111111"+dataSource.getClass()+"1111111111111111111");
+        Connection connection=dataSource.getConnection();
+        System.out.println("1111111111111111111"+connection+"1111111111111111111");
+        connection.close();
     }
 
 }
